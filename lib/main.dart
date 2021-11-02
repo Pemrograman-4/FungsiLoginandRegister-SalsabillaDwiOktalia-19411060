22c9c8e63bea5d62ce3e86f92523bef7b8514a15
SalsabillaDwiOktalia <dwioktalia86@gmail.com>
2021-11-03 01:17:05 +0700

    fungsiloginandregister

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:salsabilla_19411060/ui/login.dart';
+import 'package:salsabilla_19411060/ui/startup.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: StartUp()
+    );
+  }
+}
